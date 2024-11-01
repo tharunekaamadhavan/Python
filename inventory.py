1a5322ec495f54a3d5cb9e41b54692d3f0b6f40c
tharunekaamadhavan <tharunekaamadhavan@outlook.com>
2024-11-01 20:31:57 +0530

    final

--- inventory.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-data1=pd.read_csv('inventory.csv')
-print(data1)
-data2=pd.read_csv('inventory2.csv')
-print(data2)
-merged_data = pd.merge(data1, data2, on='Product ID')
-print(merged_data)
